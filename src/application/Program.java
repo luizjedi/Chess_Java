9e15a6fdc838c6d36a40a27c4a28bc7487e352e0
luizjedi <luiz.pepe@hotmail.com>
2021-04-05 18:00:00 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,11 +42,19 @@ public class Program {
 
 					captured.add(capturedPiece);
 				}
-				
+
 				if (chessMatch.getPromoted() != null) {
-					
+
 					System.out.print("Enter piece for promotion (B/H/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase(); // converte para maiúscula
+
+					while (!type.equals("B") && !type.equals("H") && !type.equals("R") && !type.equals("Q")) {
+
+						System.out.print("Invalid value! Enter piece for promotion (B/H/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+
+					}
+
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
